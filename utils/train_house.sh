9d68d99e6e44f790b08edf66a1f0a6611f74c58d
Lucas Hahn <lhahn1@me.com>
2021-08-13 18:41:17 +0000

    Automate data augmentation for house dataset

--- utils/train_house.sh
+++ utils/train_house.sh
@@ -10,4 +10,5 @@ rm house/train/*
 python utils/symlink.py house/dense/images house/train
 mv house/tmp/* house/train
 rm -rf house/tmp
-python train.py -c configs/DONeRF_2_samples.ini --data house --logDir logs
+gsutil cp gs://lucas.netdron.es/dataset_info.json house
+python src/train.py -c configs/DONeRF_2_samples.ini --data house --logDir logs
