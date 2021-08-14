551a372040e362e9b448de8d4db4335b80745cdf
Lucas Hahn <lhahn1@me.com>
2021-08-14 02:53:09 +0000

    wip

--- utils/symlink.py
+++ utils/symlink.py
@@ -4,8 +4,20 @@ import sys
 if __name__ == '__main__':
 
     image_dir = os.path.abspath(sys.argv[1])
-    out_dir = sys.argv[2]
+    workspace_dir = sys.argv[2]
+    train_dir = os.path.join(workspace_dir, 'train')
+    test_dir = os.path.join(workspace_dir, 'test')
+    val_dir = os.path.join(workspace_dir, 'val')
 
     images = os.listdir(image_dir)
     for im in images:
-        os.symlink(os.path.join(image_dir, im), os.path.join(out_dir, im))
+        basename = im.split('.')[0]
+        if basename in os.listdir(train_dir):
+            os.remove(os.path.join(train_dir, basename))
+            os.symlink(os.path.join(image_dir, im), os.path.join(train_dir, im))
+        elif basename in os.listdir(test_dir):
+            os.remove(os.path.join(test_dir, basename))
+            os.symlink(os.path.join(image_dir, im), os.path.join(test_dir,im))
+        elif basename in os.listdir(val_dir):
+            os.remove(os.path.join(val_dir, basename))
+            os.symlink(os.path.join(image_dir, im), os.path.join(val_dir, im))
--- utils/train_house.sh
+++ utils/train_house.sh
@@ -4,11 +4,6 @@ python utils/generate_poses.py house
 python utils/data_loader.py house
 python utils/jpg_to_png.py house/dense/images
 rm -f house/dense/images/*.jpg
-mkdir -p house/tmp
-mv house/train/*.npz house/tmp
-rm house/train/*
-python utils/symlink.py house/dense/images house/train
-mv house/tmp/* house/train
-rm -rf house/tmp
+python utils/symlink.py house/dense/images house
 gsutil cp gs://lucas.netdron.es/dataset_info.json house
 python src/train.py -c configs/DONeRF_2_samples.ini --data house --logDir logs
